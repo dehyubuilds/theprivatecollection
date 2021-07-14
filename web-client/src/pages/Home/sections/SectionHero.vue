94a38d68d8601d02361c263ddb7e352c029c4c4a
Dehyu Sinyan <dehsin@Dehyus-Mac-mini.attlocal.net>
2021-07-14 13:47:40 -0500

    changed text

--- web-client/src/pages/Home/sections/SectionHero.vue
+++ web-client/src/pages/Home/sections/SectionHero.vue
@@ -2,9 +2,7 @@
   <section class="SectionHero" v-once>
     <Container>
       <Heading level="1">The Private Collection</Heading>
-      <Text class="SectionHero__description">
-        Upload and share files quickly and securely.
-      </Text>
+      <Text class="SectionHero__description"> Upload and share files quickly and securely. </Text>
 
       <div class="SectionHero__actions">
         <Button theme="primary" data-testid="primary-button" @click="navigateToUpload">
