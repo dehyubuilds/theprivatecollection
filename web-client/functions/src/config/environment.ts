4b23a793f4a21d3e41d92104752a898aa3c4db40
Dehyu Sinyan <dehyubuilds@gmail.com>
2021-07-27 18:57:47 -0400

    sweitched back to original

--- web-client/functions/src/config/environment.ts
+++ web-client/functions/src/config/environment.ts
@@ -8,8 +8,8 @@ interface IEnvironment {
 
 function createEnv (): Readonly<IEnvironment> {
   const appPort = Number(process.env.PORT) || 3333
-  const appUrl = process.env.APP_URL || `https://theprivatecollection.co:${appPort}`
-  const mongoUrl = process.env.MONGO_URL || 'mongodb://theprivatecollection.co'
+  const appUrl = process.env.APP_URL || `http://localhost:${appPort}`
+  const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/clippy-cloud'
 
   return {
     appPort,
--- web-client/src/configs/environment.ts
+++ web-client/src/configs/environment.ts
@@ -1,5 +1,5 @@
 export const environment = Object.freeze({
   appTitle: process.env.VUE_APP_TITLE || 'The Private Collection',
   appUrl: process.env.VUE_APP_BASE_URL || '/',
-  apiUrl: process.env.VUE_APP_API_URL || 'https://theprivatecollection.co',
+  apiUrl: process.env.VUE_APP_API_URL || 'http://localhost:3333',
 })
