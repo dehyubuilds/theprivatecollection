b2159a16fb01afcbedf320d83026abcb55d1f981
Dehyu Sinyan <dehyubuilds@gmail.com>
2021-07-27 21:48:59 -0400

    removed functions

--- web-client/functions/constants.ts
+++
@@ -1,4 +0,0 @@
-import path from 'path'
-
-export const FILE_MAX_SIZE = 104857600 // 100 MB
-export const UPLOAD_FOLDER_PATH = path.resolve(__dirname, '..', 'uploads')
--- web-client/functions/controllers/WakeUpController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-
-export const WakeUpController = {
-  index (_request: Request, response: Response) {
-    return response.status(204).send()
-  },
-}
