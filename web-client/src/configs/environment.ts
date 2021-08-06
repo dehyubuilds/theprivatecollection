04ebb7417d86aa5853923afb9a682c594a84a85d
Dehyu Sinyan <dehsin@Dehyus-Mac-mini.attlocal.net>
2021-08-06 15:17:56 -0500

    url updated

--- web-client/src/configs/environment.ts
+++ web-client/src/configs/environment.ts
@@ -1,5 +1,5 @@
 export const environment = Object.freeze({
   appTitle: process.env.VUE_APP_TITLE || 'The Private Collection',
-  appUrl: process.env.VUE_APP_BASE_URL || '/',
+  appUrl: process.env.VUE_APP_BASE_URL || 'theprivatecolletion.co',
   apiUrl: 'silly-jackson-36dd03.netlify.app/.netlify/functions/server',
 })
