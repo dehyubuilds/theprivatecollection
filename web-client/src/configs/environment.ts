e32b1d67849fb54360e5ffa6b59cd7dac78e9e37
Dehyu Sinyan <dehsin@Dehyus-Mac-mini.attlocal.net>
2021-08-09 14:41:59 -0500

    added env

--- web-client/src/configs/environment.ts
+++ web-client/src/configs/environment.ts
@@ -1,5 +1,5 @@
 export const environment = Object.freeze({
   appTitle: process.env.VUE_APP_TITLE || 'The Private Collection',
   appUrl: process.env.VUE_APP_BASE_URL || '/',
-  apiUrl: 'https://ec2-3-236-123-21.compute-1.amazonaws.com/',
+  apiUrl: 'https://ec2-3-236-123-21.compute-1.amazonaws.com',
 })
