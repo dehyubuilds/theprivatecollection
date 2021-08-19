ed2ad7e1d34f0f5facc72ac4e94a844c17b0ae0d
Dehyu Sinyan <dehsin@Dehyus-Mac-mini.attlocal.net>
2021-08-19 11:15:52 -0500

    upped the bytes

--- web-client/src/modules/file/constants.ts
+++ web-client/src/modules/file/constants.ts
@@ -1,5 +1,5 @@
 import { getReadableSize } from '@/modules/file'
 
-export const FILE_MAX_SIZE = 300000000 // 300 MB
+export const FILE_MAX_SIZE = 3000000000 // 300 MB
 
 export const FILE_MAX_SIZE_FORMATTED = getReadableSize(FILE_MAX_SIZE).toLowerCase().replace(' ', '')
