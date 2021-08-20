294d421312815dc69291eeedca3644ad4c7a6668
Dehyu Sinyan <dehsin@Dehyus-Mac-mini.attlocal.net>
2021-08-19 22:06:41 -0500

    changed header

--- web-client/src/pages/Home/sections/SectionHero.vue
+++ web-client/src/pages/Home/sections/SectionHero.vue
@@ -2,7 +2,7 @@
   <section class="SectionHero" v-once>
     <Container>
       <Heading level="1">The Private Collection</Heading>
-      <Text class="SectionHero__description"> Upload and share files quickly and securely. </Text>
+      <Text class="SectionHero__description"> Not your average file storage. </Text>
 
       <div class="SectionHero__actions">
         <Button theme="primary" data-testid="primary-button" @click="navigateToUpload">
