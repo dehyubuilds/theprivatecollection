43a75cbc85755a9d9cd1f06ac58ae527f110b274
Dehyu Sinyan <dehsin@dehyus-mac-mini.lan>
2021-08-31 23:57:00 -0500

    Updated tagline

--- web-client/src/pages/Home/sections/SectionHero.vue
+++ web-client/src/pages/Home/sections/SectionHero.vue
@@ -2,7 +2,7 @@
   <section class="SectionHero" v-once>
     <Container>
       <Heading level="1">The Private Collection</Heading>
-      <Text class="SectionHero__description"> Not your Average file storage. </Text>
+      <Text class="SectionHero__description"> Content Sharing made easy. </Text>
 
       <div class="SectionHero__actions">
         <Button theme="primary" data-testid="primary-button" @click="navigateToUpload">
