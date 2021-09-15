fe322b930297e5ec33e5c31a7694d5726206a66c
Dehyu Sinyan <dehsin@Dehyus-Mac-mini.attlocal.net>
2021-09-15 15:19:49 -0500

    bumped up number

--- web-client/src/modules/file/constants.ts
+++ web-client/src/modules/file/constants.ts
@@ -1,5 +1,5 @@
 import { getReadableSize } from '@/modules/file'
 
-export const FILE_MAX_SIZE = 300000000000 // 300 MB
+export const FILE_MAX_SIZE = 500000000000 // 300 MB
 
 export const FILE_MAX_SIZE_FORMATTED = getReadableSize(FILE_MAX_SIZE).toLowerCase().replace(' ', '')
